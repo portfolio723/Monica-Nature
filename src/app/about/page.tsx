e526529e3ab53872f0272b89dd95cee9ed686903
Sandeep <agencysandesigns@gmail.com>
2025-09-30 05:29:32 +0000

    increase the height of the image of the hero section in the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
   return (
     <div className="bg-background text-foreground">
       <main>
-        <section className="relative h-[50vh] w-full md:h-[75vh]">
+        <section className="relative h-[60vh] w-full md:h-[85vh]">
           {aboutImages.hero && (
             <Image
               src={aboutImages.hero.imageUrl}
